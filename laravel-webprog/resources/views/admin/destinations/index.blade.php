3cf7a31f1bb8c2f3efbfe1f97ee2db19aa4149d7
jerrold97 <jerrold97@yahoo.com>
2017-07-06 02:59:42 +0800

    Updated views\destinations\index.blade.php

--- laravel-webprog/resources/views/admin/destinations/index.blade.php
+++ laravel-webprog/resources/views/admin/destinations/index.blade.php
@@ -1,5 +1,6 @@
 @extends('layouts.app')
 @section('content')
+<div class="container" id="app">
     <div>
     @if(Session::has('info'))
         <div class="row">
@@ -10,7 +11,7 @@
     @endif
     <div class="row">
         <div class="col-md-12">
-            <a href="{{ route('destination.create') }}" class="btn btn-success">New Post</a>
+            <a href="{{ route('destination.create') }}" class="btn btn-success" @click="modal"> @{{ message }}</a>
         </div>
     </div>
     <hr>
@@ -23,4 +24,27 @@
     </div>
     @endforeach
     </div>
+</div>
+
+@endsection
+
+
+@section('modals')
+    <div class="modal fade" tabindex="-1" role="dialog" id="modal">
+        <div id="modal-dialog" class="modal-dialog  modal-lg" role="document">
+        
+        </div><!-- /.modal-dialog -->
+    </div><!-- /.modal -->
+
+@endsection
+
+@section('scripts')
+<script>
+new Vue({
+   el: '#app',
+   data: {
+   message: 'Add'
+   }
+});
+</script>
 @endsection
