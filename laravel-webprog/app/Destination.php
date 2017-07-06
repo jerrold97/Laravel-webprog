718d58f6c7a81c2d424b1e46798720b6107be334
jerrold97 <jerrold97@yahoo.com>
2017-07-07 00:42:51 +0800

    Updated Destination model

--- laravel-webprog/app/Destination.php
+++ laravel-webprog/app/Destination.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Destination extends Model
 {
+		protected $primaryKey = 'destinations_id';
 	  protected $softDelete = true; 
      protected $fillable = ['dname', 'dlocation', 'ddesc'];
 }
