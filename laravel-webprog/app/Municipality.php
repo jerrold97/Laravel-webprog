73e5b5b4e2b8d6b5a684ff666c21e5fd82fa0c50
jerrold97 <jerrold97@yahoo.com>
2017-07-07 00:43:28 +0800

    Updated Municipality.php

--- laravel-webprog/app/Municipality.php
+++ laravel-webprog/app/Municipality.php
@@ -6,5 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Municipality extends Model
 {
-    //
+		protected $primaryKey = 'municipality_id';
+	  protected $softDelete = true; 
+     protected $fillable = ['municipality'];
 }
