16a939264f198a6cb5e5b79203c2d70f18ba73e3
jerrold97 <jerrold97@yahoo.com>
2017-07-07 00:46:45 +0800

    Updated web.php

--- laravel-webprog/routes/web.php
+++ laravel-webprog/routes/web.php
@@ -16,10 +16,24 @@ Route::get('/', function () {
 });
 Route::group(['prefix' => 'admin'], function(){
 
+	Route::resource('province', 'ProvinceController');
+	Route::get('/province-table/', 'ProvinceController@table')->name('province.table');
+
+	Route::resource('municipality', 'MunicipalityController');
+	Route::get('/municipality-table/', 'MunicipalityController@table')->name('municipality.table');
+
+	Route::resource('barangay', 'BarangayController');
+	Route::get('/barangay-table/', 'BarangayController@table')->name('barangay.table');
+
 	Route::resource('destination', 'DestinationController');
 	Route::get('/destination-table/', 'DestinationController@table')->name('destination.table');
+
 	Route::resource('official', 'OfficialController');
+	Route::get('/official-table/', 'OfficialController@table')->name('official.table');
+
+
 });
+
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
