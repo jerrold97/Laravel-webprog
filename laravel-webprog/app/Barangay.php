ebb3088c68e0022f13cf5668af7c136a9bbe98d7
jerrold97 <jerrold97@yahoo.com>
2017-07-07 06:21:14 +0800

    Updated Barangay model

--- laravel-webprog/app/Barangay.php
+++ laravel-webprog/app/Barangay.php
@@ -6,5 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Barangay extends Model
 {
-    //
+		protected $primaryKey = 'barangays_id';
+	  protected $softDelete = true; 
+     protected $fillable = ['barangay_name', 'barangay_name'];
 }
