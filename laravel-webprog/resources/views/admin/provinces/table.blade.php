84c4a9f8e5cbaa80151592eadff0e19f2115c38c
jerrold97 <jerrold97@yahoo.com>
2017-07-11 02:18:48 +0800

    Updated admin\provinces\table.blade.php

--- laravel-webprog/resources/views/admin/provinces/table.blade.php
+++ laravel-webprog/resources/views/admin/provinces/table.blade.php
@@ -12,9 +12,11 @@
         <td>{{ $province->province }}</td>
         <td>{{ $province->capital}}</td>
         <td class="">
-            <div class="delete-modal btn-sm btn-danger pull-right delete" data-id="{{$province->provinces_id}}">
+        <button class="btn btn-danger" data-toggle="modal" data-target="#">Edit</button>
+        <button class="btn btn-alert" data-toggle="modal" data-target="#">Delete</button>
+           <a href="{{ route('province.show',$province->provinces_id)}}"> <div class="delete-modal btn-sm btn-danger pull-right delete" data-id="{{$province->provinces_id}}">
                 <i class="voyager-eye"></i> Municipalities
-            </div>
+            </div></a>
         </td>
     </tr>
 	@endforeach
