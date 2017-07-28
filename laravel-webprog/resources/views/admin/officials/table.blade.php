f1fbac42f34eaecce1945e9bb23394de7acc297c
jerrold97 <jerrold97@yahoo.com>
2017-07-29 00:51:31 +0800

    Updated officials\table.blade.php

---
+++ laravel-webprog/resources/views/admin/officials/table.blade.php
@@ -0,0 +1,30 @@
+<table id="dataTable" class="table table-hover">
+<thead>
+    <tr>
+		<th>Province</th>
+        <th>Municipality/City</th>
+        <th>Name</th>
+      <th class="no-sort sorting_disabled actions">Actions</th>
+    </tr>
+</thead>
+<tbody>
+	@foreach($officials as $official)
+    <tr>
+        <td>{{ $official->fkofficial_municipality }}</td>
+        <td>{{ $official->fkofficial_province}}</td>
+        <td>{{ $official->fullName()}}</td>
+        <td class="">
+            <div class="delete-modal btn-sm btn-danger pull-right delete" data-id="{{$official->official_id}}">
+                <i class="voyager-trash"></i> Delete
+            </div>
+            <a data-id="{{$official->official_id}}" href="#" class="btn-sm btn-primary pull-right edit">
+                <i class="voyager-edit"></i> Edit
+            </a>
+            <a data-id="{{$official->official_id}}" href="#" class="btn-sm btn-warning pull-right view">
+                <i class="voyager-eye"></i> View
+            </a>
+        </td>
+    </tr>
+	@endforeach
+</tbody>
+</table>
