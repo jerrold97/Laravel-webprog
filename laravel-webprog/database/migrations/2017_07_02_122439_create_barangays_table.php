53d5a2a54dff64fcebf92a615b5be517782ff642
jerrold97 <jerrold97@yahoo.com>
2017-07-28 23:00:29 +0800

    Updated barangays table

--- laravel-webprog/database/migrations/2017_07_02_122439_create_barangays_table.php
+++ laravel-webprog/database/migrations/2017_07_02_122439_create_barangays_table.php
@@ -14,12 +14,14 @@ class CreateBarangaysTable extends Migration
     public function up()
     {
         Schema::create('barangays', function (Blueprint $table) {
+            $table->engine = 'InnoDB';
             $table->increments('barangays_id');
             $table->integer('fkbarangays_municipalities')->unsigned();
+            $table->foreign('fkbarangays_municipalities')->references('municipality_id')->on('municipalities');
             $table->text('barangay_name');
             $table->text('barangay_desc');
             $table->timestamps();
-            $table->foreign('fkbarangays_municipalities')->references('municipality_id')->on('municipalities');
+
         });
     }
 
