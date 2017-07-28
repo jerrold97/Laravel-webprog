00a12f953c06c5f582e2b981f3045fb743e5b3f2
jerrold97 <jerrold97@yahoo.com>
2017-07-28 23:15:17 +0800

    Updated officials migration

--- laravel-webprog/database/migrations/2017_07_03_171539_create_officials_table.php
+++ laravel-webprog/database/migrations/2017_07_03_171539_create_officials_table.php
@@ -14,7 +14,14 @@ class CreateOfficialsTable extends Migration
     public function up()
     {
         Schema::create('officials', function (Blueprint $table) {
-            $table->increments('id');
+            $table->increments('official_id');
+            $table->integer('fkofficial_municipality')->unsigned();
+            $table->foreign('fkofficial_municipality')->references('municipality_id')->on('municipalities');
+            $table->integer('fkofficial_province')->unsigned();
+            $table->foreign('fkofficial_province')->references('provinces_id')->on('provinces');
+            $table->string('official_first',45);
+            $table->string('official_middle',45)->nullable();;
+            $table->string('official_last',45);
             $table->timestamps();
         });
     }
