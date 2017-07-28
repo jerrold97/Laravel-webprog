6b326dc969715fd519034d5ad1c4261cd64ac122
jerrold97 <jerrold97@yahoo.com>
2017-07-29 00:50:36 +0800

    Official model update

--- laravel-webprog/app/Official.php
+++ laravel-webprog/app/Official.php
@@ -6,5 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Official extends Model
 {
-    //
+	 protected $primaryKey = 'official_id';
+	 protected $softDelete = true; 
+     protected $fillable = ['fkofficial_municipality', 'fkofficial_province', 'official_first', 'official_middle', 'official_last'];
+     
+    public function fullName()
+    {
+        return $this->official_first.', '.$this->official_middle.' '.$this->official_last;
+    }
 }
