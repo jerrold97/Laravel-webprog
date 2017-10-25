3c34f9ae94738f14ab23817f7b03b6e41617234d
jerrold97 <jerrold97@yahoo.com>
2017-10-25 17:06:44 +0800

    destinations migration update

--- laravel-webprog/database/migrations/2017_07_03_165358_create_destinations_table.php
+++ laravel-webprog/database/migrations/2017_07_03_165358_create_destinations_table.php
@@ -16,15 +16,15 @@ class CreateDestinationsTable extends Migration
         Schema::create('destinations', function (Blueprint $table) {
             $table->increments('destinations_id');
             $table->integer('fkdestination_barangays')->unsigned();
-            $table->integer('fkdestination_municipalities')->unsigned();
-            $table->integer('fkdestination_provinces')->unsigned();
+            // $table->integer('fkdestination_municipalities')->unsigned();
+            // $table->integer('fkdestination_provinces')->unsigned();
             $table->string('dname',45);
             $table->string('dlocation',45);
             $table->string('ddesc',45);
     
             $table->foreign('fkdestination_barangays')->references('barangays_id')->on('barangays');
-            $table->foreign('fkdestination_municipalities')->references('municipality_id')->on('municipalities');
-            $table->foreign('fkdestination_provinces')->references('provinces_id')->on('provinces');
+            // $table->foreign('fkdestination_municipalities')->references('municipality_id')->on('municipalities');
+            // $table->foreign('fkdestination_provinces')->references('provinces_id')->on('provinces');
             
             $table->timestamps();
         });
