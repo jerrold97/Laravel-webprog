2b93ddd2f412d40a64e00ccb1eb02ef94215b4df
jerrold97 <jerrold97@yahoo.com>
2017-10-25 15:35:22 +0800

    Destination relationship to Barangay

--- laravel-webprog/app/Destination.php
+++ laravel-webprog/app/Destination.php
@@ -6,7 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Destination extends Model
 {
-		protected $primaryKey = 'destinations_id';
-	  protected $softDelete = true; 
-     protected $fillable = ['fkdestination_barangays','dname', 'dlocation', 'ddesc'];
+	protected $primaryKey = 'destinations_id';
+	protected $softDelete = true; 
+	protected $fillable = ['fkdestination_barangays','dname', 'dlocation', 'ddesc'];
+	
+	public function barangay() {
+		return $this->belongsTo('App\Barangay', 'fkdestination_barangays', 'barangays_id');
+	}
 }
