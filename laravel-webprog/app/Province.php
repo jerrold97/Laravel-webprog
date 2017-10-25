849364c5cef1d62a06604c868fccbaae5b7429ab
jerrold97 <jerrold97@yahoo.com>
2017-10-25 15:36:12 +0800

    Province update

--- laravel-webprog/app/Province.php
+++ laravel-webprog/app/Province.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Province extends Model
 {
-	  protected $softDelete = true; 
+     protected $softDelete = true; 
+     protected $primaryKey = 'provinces_id';
      protected $fillable = ['province', 'capital'];
 }
