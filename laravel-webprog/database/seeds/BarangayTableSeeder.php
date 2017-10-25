a4a82e6acd5d5774925c94ec8ed905104ef74881
jerrold97 <jerrold97@yahoo.com>
2017-10-25 17:06:56 +0800

    BarangayTableSeeder update

--- laravel-webprog/database/seeds/BarangayTableSeeder.php
+++ laravel-webprog/database/seeds/BarangayTableSeeder.php
@@ -11,6 +11,7 @@ class BarangayTableSeeder extends Seeder
      */
     public function run()
     {
+//Albay - Bacacay
         $barangay = new \App\Barangay([
         	'fkbarangays_municipalities' => "1",
         	'barangay_name' => 'Baclayon',
@@ -50,6 +51,51 @@ class BarangayTableSeeder extends Seeder
          $barangay = new \App\barangay([
          	'fkbarangays_municipalities' => "1",
     	'barangay_name' => 'Barangay 4',
+    	'barangay_desc' => 'Barangay 4'
+        ]);
+        $barangay->save();
+
+
+//Albay - Camalig
+        $barangay = new \App\Barangay([
+        	'fkbarangays_municipalities' => "2",
+        	'barangay_name' => 'Anoling',
+        	'barangay_desc' => 'Barangay Anoling'
+        ]);
+
+        $barangay->save();
+
+        $barangay = new \App\barangay([
+        	'fkbarangays_municipalities' => "2",
+        	'barangay_name' => 'Baligang',
+        	'barangay_desc' => 'Barangay Baligang'
+        ]);
+        $barangay->save();
+
+        $barangay = new \App\barangay([
+        	'fkbarangays_municipalities' => "2",
+        	'barangay_name' => 'Bantonan',
+        	'barangay_desc' => 'Barangay Bantonan'
+        ]);
+        $barangay->save();
+
+         $barangay = new \App\barangay([
+         	'fkbarangays_municipalities' => "2",
+    	'barangay_name' => 'Barangay 1',
+    	'barangay_desc' => 'Barangay 1'
+        ]);
+        $barangay->save();
+
+         $barangay = new \App\barangay([
+         	'fkbarangays_municipalities' => "2",
+    	'barangay_name' => 'Barangay 2',
+    	'barangay_desc' => 'Barangay 2'
+        ]);
+        $barangay->save();
+
+         $barangay = new \App\barangay([
+         	'fkbarangays_municipalities' => "2",
+    	'barangay_name' => 'Barangay 3',
     	'barangay_desc' => 'Barangay 4'
         ]);
         $barangay->save();
