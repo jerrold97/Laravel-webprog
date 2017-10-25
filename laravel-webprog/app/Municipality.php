9601030bf347ba7dbac5f1b662aa7a80097251f6
jerrold97 <jerrold97@yahoo.com>
2017-10-25 15:35:55 +0800

    Municipality relationship to Province

--- laravel-webprog/app/Municipality.php
+++ laravel-webprog/app/Municipality.php
@@ -6,7 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Municipality extends Model
 {
-		protected $primaryKey = 'municipality_id';
-	  protected $softDelete = true; 
-     protected $fillable = ['municipality'];
+	protected $primaryKey = 'municipality_id';
+	protected $softDelete = true; 
+	protected $fillable = ['municipality'];
+	
+	public function province() {
+		return $this->belongsTo('App\Province', 'fkmunicipality_provinces', 'provinces_id');
+	}
 }
