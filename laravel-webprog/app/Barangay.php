787373599c118f89928c157d485a9c9413f54d2f
jerrold97 <jerrold97@yahoo.com>
2017-10-25 15:35:44 +0800

    Barangay relationship to Municipality

--- laravel-webprog/app/Barangay.php
+++ laravel-webprog/app/Barangay.php
@@ -6,7 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Barangay extends Model
 {
-		protected $primaryKey = 'barangays_id';
-	  protected $softDelete = true; 
-     protected $fillable = ['barangay_name', 'barangay_desc'];
+	protected $primaryKey = 'barangays_id';
+	protected $softDelete = true; 
+	protected $fillable = ['fkbarangays_municipalities', 'barangay_name', 'barangay_desc'];
+	
+	public function municipality() {
+		return $this->belongsTo('App\Municipality', 'fkbarangays_municipalities', 'municipality_id');
+	}
+
 }
