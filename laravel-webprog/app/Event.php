6a26bba77a6f6d359b569e7a317c217907cb445c
jerrold97 <jerrold97@yahoo.com>
2017-10-27 13:33:02 +0800

    Event relationship

--- laravel-webprog/app/Event.php
+++ laravel-webprog/app/Event.php
@@ -9,4 +9,8 @@ class Event extends Model
 	protected $primaryKey = 'event_id';
 	protected $softDelete = true; 
     protected $fillable = ['event_name','event_desc', 'fkevent_municipality'];
+
+	public function municipality() {
+		return $this->belongsTo('App\Municipality', 'fkevent_municipality', 'municipality_id');
+	}
 }
