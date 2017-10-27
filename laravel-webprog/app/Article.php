a5f9d9ccde0fa8f36fc0e262270ecabbe5fd05cb
jerrold97 <jerrold97@yahoo.com>
2017-10-27 12:55:37 +0800

    Article relationship

--- laravel-webprog/app/Article.php
+++ laravel-webprog/app/Article.php
@@ -9,4 +9,8 @@ class Article extends Model
 	protected $primaryKey = 'article_id';
 	protected $softDelete = true; 
     protected $fillable = ['article_name','article_desc', 'fkarticle_municipalities'];
+
+	public function municipality() {
+		return $this->belongsTo('App\Municipality', 'fkarticle_municipalities', 'municipality_id');
+	}
 }
