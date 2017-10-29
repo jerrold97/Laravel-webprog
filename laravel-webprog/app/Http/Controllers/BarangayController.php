1a895baae57cab90ce31e950f4c04e7c3d0a58f8
jerrold97 <jerrold97@yahoo.com>
2017-10-30 00:58:22 +0800

    BarangayController - WIP query

--- laravel-webprog/app/Http/Controllers/BarangayController.php
+++ laravel-webprog/app/Http/Controllers/BarangayController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Province;
+use App\Municipality;
 use App\Barangay;
 use Illuminate\Http\Request;
 
@@ -14,7 +16,8 @@ class BarangayController extends Controller
      */
     public function index()
     {
-        //
+        $barangays = Barangay::all();
+        return view('admin.barangays.index')->with('barangays',$barangays);
     }
 
     /**
@@ -26,7 +29,75 @@ class BarangayController extends Controller
     {
         //
     }
+    public function table(Request $request){
+        if($request->ajax()) {
+            $barangays = Barangay::all();
+            return view('admin.barangays.table')->with('barangays', $barangays); 
+        }
+        else {
+            return redirect(route('destination.index'));
+        }
+    }
+    public function tableProvince(Request $request,$id){
+        if($request->ajax()){
+            
+            $municipality_id = Municipality::where('fkmunicipality_provinces', $id)->pluck('municipality_id');
+            $municipalities = Municipality::where('fkmunicipality_provinces', $id)->pluck('municipality_id');
 
+            $barangays = Barangay::whereIn('fkbarangays_municipalities', $municipality_id)->pluck('barangays_id');
+            //$barangays = Destination::whereIn('fkdestination_barangays', $barangays)->get();
+
+            return view('admin.barangays.table')->with('barangays', $barangays); 
+        }
+        else {
+            return redirect(route('destination.index'));
+        }
+    }
+
+    public function tableMunicipality(Request $request, $province, $municipality){
+
+        if($request->ajax()) {
+           
+            if($municipality == 0) {
+                $municipality_id = Municipality::where('fkmunicipality_provinces', $province)->pluck('municipality_id');
+                $barangays = Barangay::whereIn('fkbarangays_municipalities', $municipality_id)->pluck('barangays_id');
+                //$barangays = Destination::whereIn('fkdestination_barangays', $barangays)->get();
+            }
+            else {
+                $barangays = Barangay::where('fkbarangays_municipalities', $municipality)->pluck('barangays_id');
+                //$barangays = Destination::whereIn('fkdestination_barangays', $barangays)->get();                
+            }
+
+            return view('admin.barangays.table')->with('barangays', $barangays); 
+        }
+        else {
+            return redirect(route('destination.index'));
+        }
+
+
+    }
+
+    public function tableBarangay(Request $request, $province, $municipality, $barangay){
+        if($request->ajax()){
+            
+            if($barangay == 0)
+            {
+                $barangays = Barangay::where('fkbarangays_municipalities', $municipality)->pluck('barangays_id');
+
+                $destinations = Destination::whereIn('fkdestination_barangays', $barangays)->get();      
+            }
+            else
+            {
+                $destinations = Destination::where('fkdestination_barangays', $barangay)->get();
+            }
+            
+
+            return view('admin.destinations.table')->with('destinations', $destinations); 
+        }
+        else {
+            return redirect(route('destination.index'));
+        }
+    }
     /**
      * Store a newly created resource in storage.
      *
