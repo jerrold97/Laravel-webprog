0e0c497ca10184d38897bdc9b6aece13917d1a9d
jerrold97 <jerrold97@yahoo.com>
2017-10-31 00:08:23 +0800

    Article Controller - updated resource functions

--- laravel-webprog/app/Http/Controllers/ArticleController.php
+++ laravel-webprog/app/Http/Controllers/ArticleController.php
@@ -70,7 +70,24 @@ class ArticleController extends Controller
 
 
     }
-
+    
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        $provinces = Province::all();
+        $municipalities = Municipality::all();
+        return view('admin.articles.form')  
+                    ->with('provinces', $provinces)
+                    ->with('municipalities', $municipalities)
+                    ->with('type', "CREATE")
+                    ->with('title', "ADD")
+                    ->with('action', 'btn-success add')
+                    ->with('button_text', 'Add');
+    }
     /**
      * Store a newly created resource in storage.
      *
@@ -79,28 +96,40 @@ class ArticleController extends Controller
      */
     public function store(Request $request)
     {
-        if($request->ajax()){
+        if($request->ajax())
+        {
 
-        $this->validate($request, [
-            // Name of input in form
-            'fkarticle_municipality' => 'required',
-            'article_name' => 'required',
-            'article_desc' => 'required'
-        ]);
+            $this->validate($request, [
+                // Name of input in form
+                'fkarticle_municipality' => 'required',
+                'article_name' => 'required',
+                'article_desc' => 'required'
+            ]);
 
-        $article = new Article([
-            //Name of column in database
-            'fkarticle_municipalities' => $request->input('fkarticle_municipality'),
-            'article_name' => $request->input('article_name'),
-            'article_desc' => $request->input('article_desc')
-        ]);
-        
-        $article->save();
-        //Alert::success('Good job!')->persistent("Close");
-        //$destination->tags()->attach($request->input('tags') === null ? [] : $request->input('tags'));
-        return redirect()->route('article.index')->with('info', 'Article Added, Article name is: ' . $request->input('article_name'));
-    }
-            else {
+            $article = new Article([
+                //Name of column in database
+                'fkarticle_municipalities' => $request->input('fkarticle_municipality'),
+                'article_name' => $request->input('article_name'),
+                'article_desc' => $request->input('article_desc')
+            ]);
+            
+            //$article->save();
+            $message = $article->save() ? [
+                'message'   => "Successfully added article",
+                'alert'     => 'success'
+            ]
+            : [
+                'message'    => "Sorry it appears there was a problem 
+                                adding this article",
+                'alert' => 'error',
+            ];
+
+            return response()->json($message);
+            //Alert::success('Good job!')->persistent("Close");
+            //$destination->tags()->attach($request->input('tags') === null ? [] : $request->input('tags'));
+            //return redirect()->route('article.index')->with('info', 'Article Added, Article name is: ' . $request->input('article_name'));
+        }
+        else {
             return redirect(route('article.index'));
         }
     }
@@ -139,7 +168,8 @@ class ArticleController extends Controller
      */
     public function edit(Request $request, $id)
     {
-        if($request->ajax()) {
+        if($request->ajax()) 
+        {
 
             $article = Article::findOrFail($id);
             $provinces = Province::all();
@@ -167,21 +197,32 @@ class ArticleController extends Controller
      */
     public function update(Request $request, Article $article)
     {
-        if($request->ajax()){
-        $this->validate($request, [
-            'fkarticle_municipality' => 'required',
-            'article_name' => 'required',
-            'article_desc' => 'required'
-        ]);
+        if($request->ajax())
+        {
+            $this->validate($request, [
+                'fkarticle_municipality' => 'required',
+                'article_name' => 'required',
+                'article_desc' => 'required'
+            ]);
 
-        $article = Article::find($request->input('id'));
-        $article->fkarticle_municipalities = $request->input('fkarticle_municipality');
-        $article->article_name = $request->input('article_name');
-        $article->article_desc = $request->input('article_desc');
+            $article = Article::find($request->input('id'));
+            $article->fkarticle_municipalities = $request->input('fkarticle_municipality');
+            $article->article_name = $request->input('article_name');
+            $article->article_desc = $request->input('article_desc');
 
-        $article->update();
+            //$article->update();
+            $message = $article->update() ? [
+                'message'   => "Successfully added article",
+                'alert'     => 'success'
+            ]
+            : [
+                'message'    => "Sorry it appears there was a problem 
+                                updating this article",
+                'alert' => 'error',
+            ];
 
-        return redirect()->route('article.index')->with('info', 'Article edited for: ' . $request->input('article_name'));
+            return response()->json($message);
+            //return redirect()->route('article.index')->with('info', 'Article edited for: ' . $request->input('article_name'));
         }else{
             return redirect(route('article.index'));
         }
@@ -196,7 +237,7 @@ class ArticleController extends Controller
     public function destroy(Request $request, $id)
     {
         $article = Article::findOrFail($id);
-        $article = $article->destroy($id)
+        $message = $article->destroy($id)
             ? [
                     'message'    => "Successfully deleted article",
                     'alert' => 'success',
@@ -206,6 +247,6 @@ class ArticleController extends Controller
                     'alert' => 'error',
             ];
 
-        return response()->json($article);
+        return response()->json($message);
     }
 }
